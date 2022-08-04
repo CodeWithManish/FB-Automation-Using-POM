3af91398dcdc824b6f967a5aaff5ece8c7580fb5
Manish <manishkumar0156@gmail.com>
2022-08-04 17:15:17 +0530

    [Manish] Added: Test Facebook login page using POM Selenium java Framework

---
+++ src/test/java/com/fb/qa/testcases/LoginPageTest.java
@@ -0,0 +1,46 @@
+package com.fb.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.fb.qa.base.TestBase;
+import com.fb.qa.pages.HomePage;
+import com.fb.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase{
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest(){
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp(){
+		initialization();
+		loginPage = new LoginPage();	
+	}
+	
+	@Test(priority=1)
+	public void loginPageTitleTest(){
+		String title = loginPage.validateLoginPageTitle();
+		Assert.assertEquals(title, "Facebook – log in or sign up");
+	}
+	
+
+	@Test(priority=2)
+	public void loginTest(){
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+
+}
